a329420476bf7fef5dff43a3798496545a93e1f3
fangulo12 <fangulo@gticr.com>
2022-04-24 00:21:48 -0600

    :fire: [Remove]
    Console log

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import Template from '@templates/Template.js';
 import '@styles/main.css';
 import '@styles/vars.styl';
 
-console.log("Hello world");
-
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
